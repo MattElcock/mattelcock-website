cf34b7f4c024f8bdae55cc7278de6e51ca196116
Matthew Elcock <mattiose@outlook.com>
2020-02-29 15:14:14 +0000

    Added react frontend example

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 
 const { status } = require("./src/middleware");
 
-const port = process.env.PORT || 3000;
+const app = express();
 
 app.get("/status", status);
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
